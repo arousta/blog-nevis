88e9f52e94d7dbbd324d73853288adeba8378548
ali <ali@personal.pro>
2022-12-23 07:51:26 +0000

    Add pre-commit config, run pre-commit on repo

--- blognevis/wsgi.py
+++ blognevis/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blognevis.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blognevis.settings")
 
 application = get_wsgi_application()
