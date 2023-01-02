27547ef5a300caa5789294ddb5c380747c8e55ea
ali <ali@personal.pro>
2023-01-02 02:17:20 +0000

    Amend Post form in admin

--- blognevis/post/admin.py
+++ blognevis/post/admin.py
@@ -1,8 +1,18 @@
 from .models import Post
 
 from django.contrib import admin
+from django.contrib.postgres.fields import ArrayField
+from django import forms
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    readonly_fields = ["author"]
+    readonly_fields = ["id", "deleted_at", "created", "modified"]
+    raw_id_fields = ["author"]
+    formfield_overrides = {ArrayField: {"widget": forms.Textarea}}
+    fieldsets = [
+        (None, {"fields": ("id",)}),
+        ("Post content", {"fields": ("author", "title", "text", "comments")}),
+        ("Timestamps", {"fields": ("created", "modified")}),
+        ("Soft delete status", {"fields": ("is_deleted", "deleted_at")}),
+    ]
