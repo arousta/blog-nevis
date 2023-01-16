630fbabb1ad5ab762d18a369cdd4ac17b5083d8e
ali <ali@personal.pro>
2023-01-16 01:43:33 +0000

    Setup admin site for blog writers with permissions

--- blognevis/author/admin.py
+++ blognevis/author/admin.py
@@ -1,17 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
 from blognevis.core.admin import BlogNevisAdmin
+from blognevis.core.constants import BLOGGERS_ADMINSITE_GROUP
 from .models import Author
 
 
 @admin.register(Author)
 class BlogNevisUserAdmin(BlogNevisAdmin, UserAdmin):
-    readonly_fields = ["date_joined"]
+    readonly_fields = ["date_joined", *BlogNevisAdmin.readonly_fields]
 
     def save_model(self, request, obj, form, change):
+        """Automatically add user to the Bloggers group and give staff status when it's created via the admin site"""
+        obj.is_staff = True
         super().save_model(request, obj, form, change)
-        # add premissions
-
-
-# admin.site.register(Author, BlogNevisUserAdmin)
+        obj.groups.add(Group.objects.get(name=BLOGGERS_ADMINSITE_GROUP))
