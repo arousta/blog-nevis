7ff265c70882c2e9dfdbaaa5f788fc70aa9b13c6
ali <ali@personal.pro>
2023-01-28 07:01:41 +0000

    Override User model instance/queryset delete() methods

--- blognevis/author/models.py
+++ blognevis/author/models.py
@@ -1,10 +1,17 @@
-from utility.models import TimeStampedSoftDeleteUUIDMixin, SoftDeleteManager
+from utility.models import TimeStampedSoftDeleteUUIDMixin, UserSoftDeleteQuerySet
 from django.contrib.auth.models import AbstractUser, UserManager
 
 
-class AuthorManager(UserManager, SoftDeleteManager):
+# Here we have custome manager of User class by django. But also want to have a custome queryset
+# that does a soft delete by setting is_active=False. We use this technique:
+# https://docs.djangoproject.com/en/4.1/topics/db/managers/#from-queryset
+class AuthorManager(UserManager.from_queryset(UserSoftDeleteQuerySet)):
     pass
 
 
 class Author(AbstractUser, TimeStampedSoftDeleteUUIDMixin):
     objects = AuthorManager()
+
+    def delete(self, using=None, keep_parents=False):
+        self.is_active = False
+        self.save(update_fields=["is_active"])
